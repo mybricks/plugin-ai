1627e543ad36a7164cf2f0880396786e9820ca70
chemingjun <chemingjun@126.com>
2025-11-13 12:06:52 +0800

    init all

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,37 @@ import pkg from '../package.json';
 
 console.log(`%c ${pkg.name} %c@${pkg.version}`, `color:#FFF;background:#fa6400`, ``, ``);
 
-export default function note(appCtx: any): any {
+export default function pluginAI(): any {
   return {
     name: '@mybricks/plugins/ai',
-    title: 'AI助手',
+    title: 'MyBricksAI助手',
     author: 'MyBricks',
     ['author.zh']: 'MyBricks',
     version: '1.0.0',
     description: 'ai for MyBricks',
     data,
     contributes: {
-      geoView: {
-        note: {
-          type: 'default',
-          render(args: any): JSX.Element {
+      aiService: {
+        init(api) {
+          //debugger
+
+          ////TODO
+          return {
+            request() {//调用
+              return new Promise((resolve, reject) => {
+                const files = {}//返回的文件列表
+                resolve(files)
+              })
+            }
           }
+        }
+      },
+      // aiService(params,ref){
+      //   ref()
+      // },
+      aiView: {
+        render(args: any): JSX.Element {
+          debugger
         }
       }
     }
