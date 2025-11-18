919e191dbff911a214cb0a73f6060ac20ba2c7ec
leon-js <42924541+leon-js@users.noreply.github.com>
2025-11-18 20:20:40 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -102,6 +102,10 @@ export default function pluginAI({
 
           return {
             focus(params: AiServiceFocusParams) {
+              if (!params) {
+                context.currentFocus = undefined;
+                return;
+              }
               console.log("[focus - params]", params)
               if (params.comId === 'u_PtIsS') {
                 params.type = 'page';
