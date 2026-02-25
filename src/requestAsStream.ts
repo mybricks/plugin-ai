47d26904b4b5dfc9d83358de657efa67027d96b9
liubiao03 <liubiao03@kuaishou.com>
2026-02-25 17:53:21 +0800

    feat: update

--- src/requestAsStream.ts
+++ src/requestAsStream.ts
@@ -150,7 +150,7 @@ const transfromExtendParams = (extendParams: { aiRole?: string }) => {
       break;
     }
     case ["architect"].includes(aiRole): {
-      model = "google/gemini-3-pro-preview";
+      model = "google/gemini-3.1-pro-preview";
       role = "architect";
       break;
     }
