7de2c60de3ca57f63ae336e1201c3a3353a4bb5c
leon-js <42924541+leon-js@users.noreply.github.com>
2026-03-16 23:16:56 +0800

    update

--- src/agents/index.ts
+++ src/agents/index.ts
@@ -16,17 +16,19 @@ export type BuiltinAgentType = 'vibe' | 'common' | 'common-by-coding'
 export const requestAgent = (type: BuiltinAgentType | undefined, params: any) => {
   const customType = context.currentFocus?.type
 
-  params.extension = {
-    ...params.extension,
-    mentions: params.extension.mentions.map((mention: any) => {
-      return {
-        ...mention,
-        focusArea: {
-          selector: mention.focusArea.selector,
-          title: mention.focusArea.title,
+  if (params.extension?.mentions?.length) {
+    params.extension = {
+      ...params.extension,
+      mentions: params.extension.mentions.map((mention: any) => {
+        return {
+          ...mention,
+          focusArea: {
+            selector: mention.focusArea.selector,
+            title: mention.focusArea.title,
+          }
         }
-      }
-    })
+      })
+    }
   }
 
   // 1. 内置类型：vibe
